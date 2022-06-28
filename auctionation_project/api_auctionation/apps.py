aaa3cf3627899548d2679e9dd971e519b7512367
WojciakW <wojciech.wojciak22@gmail.com>
2022-06-28 12:58:02 +0000

    first commit

---
+++ auctionation_project/api_auctionation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiAuctionationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_auctionation'
---
+++ auctionation_project/app_auctionation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppAuctionationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_auctionation'
